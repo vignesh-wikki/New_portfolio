9ba4cac0b4865e545db09a20c2224d26c3f6d8c1
Vignesh <96247274+vignesh-wikki@users.noreply.github.com>
2023-05-27 09:48:15 +0530

    Update Projects.js

--- src/Projects.js
+++ src/Projects.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import { NavLink,Navigate } from "react-router-dom";
 import Nav from "./Headers/Nav";
 import logo from "./Screenshot (185).png";
 import Card from "./Components/Card";
 import React, { useState, useContext, useEffect } from "react";
 import { ThemeContext } from "./darkmode/Switcher";
-import SignFilter from "./assets/SignFilter.jpg"
+import SignFilter from "./assets/SignFilter.jpg";
 function Projects() {
   const data1 = {
     title: "Netflix Clone",
@@ -12,7 +13,7 @@ function Projects() {
       "My experience on this project was awsome in learning the react . And this my first project in react.",
     tag1: "React",
     tag2: "Styled components",
-    img:"https://logodix.com/logo/707805.jpg"
+    img: "https://logodix.com/logo/707805.jpg",
   };
   const data2 = {
     title: "Youtube",
@@ -20,7 +21,7 @@ function Projects() {
       "This is my first project in React Native, it is cross-platform framework. then I am a beginner level at this cross-platform app development. ",
     tag1: "React-native",
     tag2: "Styled components",
-    img:"https://www.pixelstalk.net/wp-content/uploads/2016/05/Youtube-Wallpapers-HD.png",
+    img: "https://www.pixelstalk.net/wp-content/uploads/2016/05/Youtube-Wallpapers-HD.png",
   };
 
   const data3 = {
@@ -29,16 +30,15 @@ function Projects() {
       "A SignFilter is a Hand recogntion tool. This my first step into the AI and ML. And this was developed on Python3. ",
     tag1: "Artificial Intelligence",
     tag2: "Machine learning",
-    img:SignFilter,
+    img: SignFilter,
   };
 
   const data4 = {
     title: "New Portfolio",
-    content:
-      "My new portfolio, it was developed by React with Tailwind css.",
+    content: "My new portfolio, it was developed by React with Tailwind css.",
     tag1: "React",
     tag2: "Tailwind css",
-    img:logo,
+    img: logo,
   };
   const state1 = {
     bg: "bg-[#f0e7db]",
@@ -61,7 +61,6 @@ function Projects() {
       setStates(state1);
     }
   }, [darkMode]);
-  
 
   return (
     <>
@@ -74,13 +73,25 @@ function Projects() {
         <div className="w-full h-full ">
           <div>
             <div className="flex justify-center h-full   flex-wrap">
-              <Card {...data1} />
-              <Card {...data2} />
-              <Card {...data3} />
-              <Card {...data4} />
+              <NavLink to="https://netflixclone-ten-gamma.vercel.app/">
+                
+                <Card {...data1} />
+              </NavLink>
+
+              <NavLink to={"https://github.com/vignesh-wikki/YoutubeClone"}>
+                <Card {...data2} />
+              </NavLink>
+              <NavLink to={"https://github.com/vignesh-wikki/SignFilter"}> <Card {...data3} /></NavLink>
+             
+              <NavLink to={"/"}>
+
+                <Card {...data4} />
+              </NavLink>
             </div>
           </div>
-          <div className={`${states.footer_text} mt-3 pb-5 flex justify-center`}>
+          <div
+            className={`${states.footer_text} mt-3 pb-5 flex justify-center`}
+          >
             <p>© 2023 Vignesh. All Rights Reserved.</p>
           </div>
         </div>
